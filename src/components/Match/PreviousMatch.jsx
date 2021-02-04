23c4aabed2d525d72e8a1721822c41eb10f704b0
Tibo-Wild <thibault.leparc45@gmail.com>
2021-02-04 18:41:58 +0100

    update all files

--- src/components/Match/PreviousMatch.jsx
+++ src/components/Match/PreviousMatch.jsx
@@ -21,12 +21,12 @@ export default function Previous() {
             <h2 className="title-match">Scores des derniers matchs</h2>
             {previousData.map((item, index) =>
             <div className="container-previous-matchs" key={index}>
-                <p>{item.date}</p>
+                <p className="date-match">{item.date}</p>
                 <img src={item.logo_dom} alt="Logo équipe domicile"/>
                 <p>{item.dom}</p>
-                <p>{item.score_dom}</p>
+                <p className="score-match">{item.score_dom}</p>
                 <p className="score-separation">-</p>
-                <p>{item.score_ext}</p>
+                <p className="score-match">{item.score_ext}</p>
                 <p>{item.ext}</p>
                 <img src={item.logo_ext} alt="Logo équipe extérieur"/>
             </div>
---
+++ src/components/Stadium/Stadium.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Stadium.css';
+
+
+export default function Stadium() {
+    return (
+        <div className="container-stadium">
+            
+        </div>
+    )
+}
